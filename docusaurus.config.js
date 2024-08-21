d515798d9e10c9455d777ba8b33b1e184a66e322
lichtvoneinzbern <licht.kumicho@gmail.com>
2024-08-21 19:06:52 +0900

    change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,126 +4,136 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'Tech Doc',
-  tagline: 'tech_doc',
-  url: ' https://lichtvoneinzbern.github.io',
-  baseUrl: '/techart_document/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+    title: 'Tech Doc',
+    tagline: 'tech_doc',
+    url: ' https://lichtvoneinzbern.github.io',
+    baseUrl: '/techart_document/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    i18n: {
+        defaultLocale: 'ja',
+        locales: ['ja'],
+    },
+
+    presets: [
+        [
+        '@docusaurus/preset-classic',
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+            docs: {
+            sidebarPath: require.resolve('./sidebars.js'),
+            // Please change this to your repo.
+            editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+            },
+            blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl:
+                'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            },
+            theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+            },
+        }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Tech Doc',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/icon.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'ドキュメント',
-          },
-          {to: '/docs/obs_studio/what_is_obs', label: 'OBS Studio', position: 'left'},
-          {
-            href: 'https://lichtvoneinzbern.github.io/profile/',
-            label: 'プロフィール（英語）',
-            position: 'right',
-          },
-          {
-            href: 'https://www.foriio.com/L1cht',
-            label: 'ポートフォリオ',
-            position: 'right',
-          },
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'Tech Doc',
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/icon.png',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'ドキュメント',
+                    },
+                    {to: '/docs/obs_studio/what_is_obs', label: 'OBS Studio', position: 'left'},
+                    {
+                        href: 'https://lichtvoneinzbern.github.io/profile/',
+                        label: 'Profile',
+                        position: 'right',
+                    },
+                    {
+                        href: 'https://www.foriio.com/L1cht',
+                        position: 'right',
+                        className: 'header-foriio-link',
+                    },
 
-          {
-            href: 'https://x.com/EldL1cht',
-            label: 'X(Twitter)',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/lichtvoneinzbern',
-            label: 'GitHub',
-            position: 'right',
-          },
+                    {
+                        href: 'https://x.com/EldL1cht',
+                        position: 'right',
+                        className: 'header-x-link',
+                    },
+                    {
+                        href: 'https://github.com/lichtvoneinzbern',
+                        position: 'right',
+                        className: 'header-github-link',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'ドキュメント',
+                        items: [
+                            {
+                                label: 'はじめに',
+                                to: '/docs/intro/',
+                            },
+                            {
+                                label: 'OBS Studio',
+                                to: '/docs/obs_studio/what_is_obs/',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'SNS',
+                        items: [
+                            {
+                                label: 'X (Twitter)',
+                                href: 'https://x.com/EldL1cht',
+                            },
+                            {
+                                label: 'Creatir Hub (Discord 情報共有サーバー)',
+                                href: 'https://discord.gg/4TK8Av7S',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'その他',
+                        items: [
+                            {
+                                label: 'プロフィール（英語）',
+                                href: 'https://lichtvoneinzbern.github.io/profile/',
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/facebook/docusaurus',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} L1cht.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
 
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'ドキュメント',
-            items: [
-              {
-                label: 'はじめに',
-                to: '/docs/intro/',
-              },
-              {
-                label: 'OBS Studio',
-                to: '/docs/obs_studio/what_is_obs/',
-              },
-            ],
-          },
-          {
-            title: 'SNS',
-            items: [
-              {
-                label: 'X (Twitter)',
-                href: 'https://x.com/EldL1cht',
-              },
-              {
-                label: 'Creatir Hub (Discord 情報共有サーバー)',
-                href: 'https://discord.gg/4TK8Av7S',
-              },
-            ],
-          },
-          {
-            title: 'その他',
-            items: [
-              {
-                label: 'プロフィール（英語）',
-                href: 'https://lichtvoneinzbern.github.io/profile/',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} L1cht.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+            colorMode: {
+                defaultMode: 'light',
+                disableSwitch: false,
+                respectPrefersColorScheme: true,
+            },
+        }),
 });
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -9,33 +9,33 @@ const FeatureList = [
     description: (
       <>
         PhotoshopやMayaなどのデザインソフト、<br/>
-        OBS Studioといった配信ソフトの設定・使い方など、<br/>
-        ドキュメントとして体系的にまとめています。
+        OBS Studioといった配信ソフトなど、<br/>
+        設定方法・使い方を体系的にまとめます。
       </>
     ),
-    imgSize: { width: 180, height: 180 },
+    imgSize: { width: 128, height: 128 },
   },
   {
     title: '現場で使える情報',
     Img: require('../../static/img/profile.png').default, // defaultを追加
     description: (
       <>
-        著者自身が第一線で活動している現役クリエイターです。<br/>
+        著者自身が現役で活動しているクリエイターです。<br/>
         私自身ないし現場のクリエイターが実際に使用する情報をまとめます。
       </>
     ),
-    imgSize: { width: 180, height: 180 },
+    imgSize: { width: 128, height: 128 },
   },
   {
     title: '情報の信頼性',
     Img: require('../../static/img/trust.png').default, // defaultを追加
     description: (
       <>
-        公式ないし信頼にたる情報元が明言している内容を引用元と共に記載し、著者の主観は別枠で記述します。<br/>
-        出典を明らかにすることで、確かな情報を提供します。
+        出典を明らかにし、確かな情報を提供します。<br/>
+        信頼にたる情報元が明言している内容を引用元と共に記載し、著者の主観は別枠で記述します。<br/>
       </>
     ),
-    imgSize: { width: 180, height: 180 },
+    imgSize: { width: 128, height: 128 },
   },
 
 ];
